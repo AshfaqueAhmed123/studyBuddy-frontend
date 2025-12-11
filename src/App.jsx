33fe6fbcd81da4bb352e911ec2646c90f33a7dc4
Ashfaque Ahmed <ashfaqueahmed2342@gmail.com>
2025-12-11 12:41:41 +0500

    setup files structure and basic navigation

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import LoginPage from "./pages/Auth/LoginPage"
+import RegisterPage from "./pages/Auth/RegisterPage"
+import NotFoundPage from "./pages/NotFoundPage"
+import DashboardPage from './pages/Dashboard/DashboardPage'
+import DocumentListPage from "./pages/Documents/DocumentListPage"
+import DocumentDetailsPage from "./pages/Documents/DocumentDetailsPage"
+import FlashcardsListPage from "./pages/FlashCards/FlashcardListPage"
+import FlashcardPage from "./pages/FlashCards/FlashcardPage"
+import QuizResultPage from "./pages/Quizzes/QuizResultPage"
+import QuizTakePage from "./pages/Quizzes/QuizTakePage"
+import ProtectedRoute from './components/auth/ProtectedRoute'
+import ProfilePage from "./pages/Profile/ProfilePage"
+
+const App = () => {
+
+  const isAuthenticated  = true;
+  const loading = false;
+
+  if (loading){
+    return (
+      <div className='flex items-center justify-center h-screen '>
+        <p>loading...</p>
+      </div>
+    );  
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+        path='/'
+        element={isAuthenticated ? <Navigate to="/dashboard" replace/> : <Navigate to="/login" replace />}
+        />
+        <Route path='/login' element={<LoginPage/>} />
+
+        <Route path='/register' element={<RegisterPage/>} />
+
+        <Route element={<ProtectedRoute/>}>
+          <Route path='/dashboard' element={<DashboardPage/>}/>
+          <Route path='/documents' element={<DocumentListPage/>} />
+          <Route path='documents/:id' element={<DocumentDetailsPage/>}/>
+          <Route path='/flashcards' element={<FlashcardsListPage/>} />
+          <Route path='/document/:id/flashcards' element={<FlashcardPage/>} />
+          <Route path='/quizzes/:quizid' element={<QuizTakePage/>} />
+          <Route path='/quizzes/:quizid/results' element={<QuizResultPage/>} />
+          <Route path='/profile' element={<ProfilePage/>} />
+
+        </Route>
+
+        <Route path='/*' element={<NotFoundPage/>} />
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App  
